547a743a6f937816c36875f0ce0d4834d3f0c822
kevingroque <kevingroque@github.com>
2017-08-29 09:24:28 -0500

    Nuevos Cambios

--- lab02ExampleGitHub03/src/main/java/com/linder/lab02ExampleGitHub03/StudentController.java
+++ lab02ExampleGitHub03/src/main/java/com/linder/lab02ExampleGitHub03/StudentController.java
@@ -4,10 +4,14 @@ package com.linder.lab02ExampleGitHub03;
  * Hello world!
  *
  */
-public class App 
+public class StudentController 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        showMessage();
+    }
+    private static void showMessage() {
+    	System.out.println("mensaje...!");
     }
 }
